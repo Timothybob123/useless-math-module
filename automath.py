6d44d1e456b2eee36fee63bbd5412d50c10c8d4d
Timothybob123 <152337984+Timothybob123@users.noreply.github.com>
2024-01-13 18:16:59 -0500

    Add files via upload

---
+++ automath.py
@@ -0,0 +1,13 @@
+def math(number1, number2):
+  question = input("symbol? ")
+  if question == "+":
+    print(number1 + number2)
+  elif question == "-":
+    print(number1 - number2)
+  elif question == "*":
+    print(number1 * number2)
+  elif question == "/":
+    print(number1 / number2)
+  
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import automath
+from automath import math
+
+automath.math(2, 2)
+#Just basic info on how to use the module.
